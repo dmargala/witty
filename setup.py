de4f328d71f01b00ffdd7a58f0ffea077fa2df82
Daniel Margala <daniel@svds.com>
2016-04-18 14:53:05 -0700

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='witty',
-    version='0.1.0',
+    version='0.1.1',
     description="witty is witty.",
     long_description=readme + '\n\n' + history,
     author="Daniel Margala",
--- witty/__init__.py
+++ witty/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Daniel Margala'
 __email__ = 'djmargala@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
